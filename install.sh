48c1bb73f0a984fa8f4f1ec9a95ba3ac59bb4930
ArataK1t <168479108+ArataK1t@users.noreply.github.com>
2024-12-01 18:46:33 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -81,6 +81,7 @@ PROXY_FILE="$HOME/proxies.txt"
 # Проверка наличия файла с прокси
 if [ ! -f "$PROXY_FILE" ]; then
   touch "$PROXY_FILE"
+  nano "$PROXY_FILE"
   error "Файл с прокси не найден. Пожалуйста, создайте файл $PROXY_FILE и введите список прокси."
   exit 1
 fi
